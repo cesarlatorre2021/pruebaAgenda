1cf8a9d663022f290788adbff7271bc2ee3c6c0c
Cesar Latorre <sfecesar1941@hotmail.com>
2022-06-30 08:01:17 -0500

    se cargan los archivos para la prueba Cardif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Agenda } from './domain/agenda';
+import { agendaServicio } from './service/agendaservicio';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  styles: [`
+  :host ::ng-deep .p-dialog .agenda-image {
+      width: 150px;
+      margin: 0 auto 2rem auto;
+      display: block;
+  }
+`],
+  providers: [MessageService,ConfirmationService,]
 })
-export class AppComponent {
-  title = 'pruebaAgenda';
+export class AppComponent implements OnInit {
+
+  agendaDialogo: boolean = false;
+  agendas: Agenda[] = [];
+  agenda!: Agenda;
+  seleccionarAgendas: Agenda[] = [];
+  presentado: boolean = false;
+  value: boolean = true;
+
+  constructor(private agendaServicio: agendaServicio, private mensaje: MessageService, private confirmationService: ConfirmationService) { }
+
+  ngOnInit() {
+      this.agendaServicio.getAgendas().then(data => this.agendas = data);
+  }
+
+  abrirNuevo() {
+      this.agenda = {};
+      this.presentado = false;
+      this.agendaDialogo = true;
+  }
+
+  editarAgenda(agenda: Agenda) {
+      this.agenda = {...agenda};
+      this.agendaDialogo = true;
+  }
+
+  cargarDialogo() {
+      this.agendaDialogo = false;
+      this.presentado = false;
+  }
+  
+  guardarAgenda() {
+      this.presentado = true;
+
+      if (this.agenda.nombres?.trim) {
+          if (this.agenda.id) {
+              this.agendas[this.buscarIndiceId(this.agenda.id)] = this.agenda;                
+              this.mensaje.add({severity:'exitosa', summary: 'Exitosa', detail: 'Agenda Creada', life: 3000});
+          }
+          else {
+              this.agenda.id = this.crearId();
+              this.agendas.push(this.agenda);
+              this.mensaje.add({severity:'exitosa', summary: 'Exitosa', detail: 'Agenda Creada', life: 3000});
+          }
+
+          this.agendas = [...this.agendas];
+          this.agendaDialogo = false;
+          this.agenda = {};
+      }
+  }
+
+  buscarIndiceId(id: string): number {
+      let index = -1;
+      for (let i = 0; i < this.agendas.length; i++) {
+          if (this.agendas[i].id === id) {
+              index = i;
+              break;
+          }
+      }
+
+      return index;
+  }
+
+  crearId(): string {
+      let id = '';
+      var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      for ( var i = 0; i < 5; i++ ) {
+          id += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+      }
+      return id;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { agendaServicio } from './service/agendaservicio';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'primeng/toast';
+import { CalendarModule } from 'primeng/calendar';
+import { SliderModule } from 'primeng/slider';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { InputTextModule } from 'primeng/inputtext';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ToolbarModule } from 'primeng/toolbar';
+import { RatingModule } from 'primeng/rating';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    TableModule,
+    HttpClientModule,
+    CalendarModule,
+    SliderModule,
+    DialogModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
+    ButtonModule,
+    ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    HttpClientModule,
+    FileUploadModule,
+    ToolbarModule,
+    RatingModule,
+    RadioButtonModule,
+    InputNumberModule,
+    ConfirmDialogModule,
+    InputTextareaModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [agendaServicio, MessageService, ConfirmationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
