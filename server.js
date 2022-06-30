11a583e532df2efffe0ac1144e1abab0668c0e55
Cesar Latorre <sfecesar1941@hotmail.com>
2022-06-30 09:15:16 -0500

    actualizaciones para Heroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/pruebaAgenda'));
+app.use(express.static(__dirname+'/dist/prueba-agenda'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/pruebaAgenda/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/prueba-agenda/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, function(){
